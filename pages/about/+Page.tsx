32c686918016ea34cd5808be8115c2c67a3dc280
Addemark <addemark@gmail.com>
2024-02-05 20:59:12 +0200

    adding fonts

--- pages/about/+Page.tsx
+++ pages/about/+Page.tsx
@@ -7,7 +7,9 @@ function Page() {
     <>
       <h1>About</h1>
       <p>Example of using Vike.</p>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline font-EudoxusSansExtraBold  mb-16">
+        Hello world!
+      </h1>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ export default {
     "./pages/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    mode: "jit",
     extend: {},
+    fontFamily: {
+      EudoxusSansBold: ["EudoxusSans-Bold", "sans-serif"],
+      EudoxusSansExtraBold: ["EudoxusSans-ExtraBold", "sans-serif"],
+      EudoxusSansExtraLight: ["EudoxusSans-ExtraLight", "sans-serif"],
+      EudoxusSansLight: ["EudoxusSans-Light", "sans-serif"],
+      EudoxusSansMedium: ["EudoxusSans-Medium", "sans-serif"],
+      EudoxusSansRegular: ["EudoxusSans-Regular", "sans-serif"],
+    },
   },
   plugins: [
     require("@tailwindcss/typography"),
