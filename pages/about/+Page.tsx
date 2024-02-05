78c22d0bab712a8d46768fc690791a19baa8656e
Addemark <addemark@gmail.com>
2024-02-05 20:40:18 +0200

    add tailwind css

--- pages/about/+Page.tsx
+++ pages/about/+Page.tsx
@@ -1,12 +1,13 @@
-import './code.css'
+import "./code.css";
 
-export { Page }
+export { Page };
 
 function Page() {
   return (
     <>
       <h1>About</h1>
       <p>Example of using Vike.</p>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
-  )
+  );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import react from '@vitejs/plugin-react'
-import vike from 'vike/plugin'
-import { UserConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import vike from "vike/plugin";
+import { UserConfig } from "vite";
 
 const config: UserConfig = {
-  plugins: [react(), vike()]
-}
+  plugins: [react(), vike()],
+  server: {
+    port: 3030,
+    // host: true,
+  },
+};
 
-export default config
+export default config;
